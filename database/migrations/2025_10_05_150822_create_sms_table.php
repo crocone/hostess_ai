bc778cc9640952836c5649b92111f38270cd24cb
shevt <shevtsov@croc.one>
2025-10-12 12:36:18 +0300

    fix

--- database/migrations/2025_10_05_150822_create_sms_table.php
+++ database/migrations/2025_10_05_150822_create_sms_table.php
@@ -33,6 +33,9 @@ return new class extends Migration {
 
     public function down(): void
     {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phone');
+        });
         Schema::dropIfExists('confirm_codes');
     }
 };
