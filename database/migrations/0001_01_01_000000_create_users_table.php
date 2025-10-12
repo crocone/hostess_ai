cad83691b5c2cc03cf4f9aeec87e14c5821a619c
shevt <shevtsov@croc.one>
2025-10-12 14:14:07 +0300

    fix: accept

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_subscribed')->default(false);
+            $table->boolean('is_accepted')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
